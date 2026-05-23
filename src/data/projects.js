2056f6dbcebe76c532e2690b07b494b898253398
JoseME1 <josemunoz210699@gmail.com>
2026-05-23 02:04:10 -0600

    fin (tal vez)

---
+++ src/data/projects.js
@@ -0,0 +1,52 @@
+// Proyectos - datos reutilizables
+const PROJECTS = [
+  {
+    id: 'proj-1',
+    title: 'Azucaris: La Aventura de Armandillo',
+    description: 'Videojuego de plataformas 2D desarrollado en Unity, con mecánicas clásicas y combates rítmicos.',
+    image: '/src/assets/Proyecto Armandillo.webp',
+    youtube: 'https://www.youtube.com/watch?v=2bY6royWxAw',
+    tags: ['Unity', 'C#', 'Pixel Art'],
+    
+  },
+  {
+    id: 'proj-2',
+    title: 'Alien Massacre 2077',
+    description: 'Videojuego de disparos en primera persona para Android desarrollado en Unity, inspirado en clásicos como Doom y un sistema de oleadas de enemigos.',
+    github: 'https://github.com/JoseME1/Alien-Massacre-2077-Final-',
+    youtube: 'https://www.youtube.com/watch?v=RZJ4Y2ZlDqQ',
+    image: '/src/assets/Proyecto AlienMassacre.webp',
+    tags: ['Unity', 'C#', 'Pixel Art'],
+    
+  },
+  {
+    id: 'proj-3',
+    title: 'UANL KART',
+    description: 'Videojuego de carreras arcade desarrollado en Unity, con personajes y escenarios inspirados en la UANL e inspirado en Mario Kart. Pensado para ser jugado con un háptico construido con Arduino.',
+    youtube: 'https://www.youtube.com/watch?v=FEHiN_YqLkA',
+    image: '/src/assets/Proyecto Uanlkart.webp',
+    tags: ['Unity', 'C#', '3D Modeling','Arduino'],
+    
+  },
+  {
+    id: 'proj-4',
+    title: 'La Calaca Chida',
+    description: 'Videojuego hecho sobre OpenGL en C++, con mecánicas de combate por turnos. ',
+    github: 'https://github.com/JoseME1/Pia-POO',
+    image: '/src/assets/Proyecto Poo.webp',
+    tags: ['OpenGL', 'C++', '3D Modeling'],
+    
+  },
+  {
+    id: 'proj-5',
+    title: 'API de Recetas',
+    description: 'Proyecto de desarrollo de API RESTful para gestionar una red social de recetas culinarias. Con CRUD de usuarios, recetas, comentarios y valoraciones.',
+    github: 'https://github.com/JoseME1/API-Recetas',
+    image: '/src/assets/Proyecto API.webp',
+    tags: ['Python', 'Flask', 'SQLAlchemy','MySQL'],
+    
+  },
+  
+];
+
+export default PROJECTS;
